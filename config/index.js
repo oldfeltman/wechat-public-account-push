7d48b85675b8f380534a81ae3b4238bacf86ce8c
oldfeltman <114303500+oldfeltman@users.noreply.github.com>
2022-09-25 02:05:58 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -218,11 +218,7 @@ const USER_CONFIG = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'Sh5yLgqNaZC84lebeZ
-
-LbvBGAcgxalutNy8H 1
-
-Pb8ltt8',
+  CALLBACK_TEMPLATE_ID: 'Sh5yLgqNaZC84lebeZLbvBGAcgxalutNy8H 1Pb8ltt8',
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
