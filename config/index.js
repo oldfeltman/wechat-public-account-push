20fb0a584e1b670327419beafab327b7ea99c9a7
oldfeltman <114303500+oldfeltman@users.noreply.github.com>
2022-09-25 10:07:59 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,12 +7,12 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: 'wxff87b3b116640b7b',
+  APP_ID: '',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '2acda4617ac2cea600470545fd3b1d06',
+  APP_SECRET: '',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'Sh5yLgqNaZC84lebeZLbvBGAcgxalutNy8H 1Pb8ltt8',
+      useTemplateId: 'Sh5yLgqNaZC84IebeZLbvBGAcgxaIutNy8H1Pb8ltt8',
       // 所在省份
       province: '宁夏',
       // 所在城市
@@ -221,7 +221,7 @@ const USER_CONFIG = {
   CALLBACK_TEMPLATE_ID: 'Sh5yLgqNaZC84lebeZLbvBGAcgxalutNy8H 1Pb8ltt8',
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
-  CALLBACK_USERS: [
+  CALLBACK_USERS: [ociCa5kXNsCPBGX-GvmIHOmurdFI
     {
       // 一般都填自己
       name: '',
@@ -265,17 +265,17 @@ const USER_CONFIG = {
   FESTIVALS: [
     // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
     {
-      type: '*02-22', name: '小曹', year: '2003', date: '02-22', isShowAge: true,
+      type: '*02-23', name: '小曹', year: '2003', date: '02-23', isShowAge: true,
     },
     {
-      type: '02-22', name: '生日', year: '2003', date: '02-22',
+      type: '02-23', name: '生日', year: '2003', date: '02-22',
     },
     // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
     {
-      type: '02-22', name: '轩轩', year: '2003', date: '02-22', isShowAge: true,
+      type: '02-23', name: '小曹', year: '2003', date: '02-23', isShowAge: true,
     },
     {
-      type: '02-22', name: '被搭讪纪念日', year: '2021', date: '09-01',
+      type: '02-23', name: '被搭讪纪念日', year: '2021', date: '09-01',
     }
     // 你可以不断按格式往下增加
     // ...
